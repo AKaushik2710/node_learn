b0017659dbc65fc9f4d73e68d6cb4e1e5b128a71
Akaushik2710 <amankaushik2710ak@gmail.com>
2025-04-25 07:18:59 +0000

    OS module learnt

---
+++ remote/os.js
@@ -0,0 +1,11 @@
+const os = require('os');
+
+console.log(os.arch()); // os methods
+console.log(os.cpus()); // cpu info
+console.log(os.freemem()); // free memory
+console.log(os.homedir()); // home directory
+console.log(os.hostname()); // hostname
+console.log(os.networkInterfaces()); // network interfaces
+console.log(os.platform()); // platform
+console.log(os.uptime()); // uptime
+console.log(os.userInfo()); // user info
