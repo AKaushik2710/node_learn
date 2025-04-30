da2a024ecbdb0aa7b2e32e38b566c8a24356132f
Akaushik2710 <amankaushik2710ak@gmail.com>
2025-04-30 15:29:44 +0000

    MongoDB Connected

---
+++ app-builder/db.js
@@ -0,0 +1,26 @@
+const { MongoClient} = require('mongodb');
+const uri = "mongodb+srv://amankaushiksplaasher:QtMRtl5F1oJoMB1l@learn.fqnoudy.mongodb.net/?retryWrites=true&w=majority&appName=learn";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("aman").command({ ping: 1 });
+//     gets = await client.db("aman").collection("db_learn").find().toArray();
+//     console.log(gets)
+//     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// Mongoose Way
+const mongoose = require('mongoose')
+
+mongoose.connect(uri).then(()=>console.log("connected")).catch((err)=>console.log(err))
